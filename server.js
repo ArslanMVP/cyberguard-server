0b127609b38001fbfef56c57e9e749cb809edb05
ArslanMVP <120491462+ArslanMVP@users.noreply.github.com>
2025-05-05 05:19:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/cyberguard', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 const UserSchema = new mongoose.Schema({
     login: { type: String, unique: true, required: true },
